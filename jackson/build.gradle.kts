6a703d56e67b7c4b650264abb4d49ee159395fe2
Eric Jessé <me@ericjesse.com>
2022-06-22 18:06:04 +0200

    Adding the gradle file missing after migration from older repository

---
+++ jackson/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.allopen")
+    `java-test-fixtures`
+}
+
+description = "Qalipsis Plugins - Jackson Serializer and Deserializer"
+
+allOpen {
+    annotations(
+        "io.micronaut.aop.Around",
+        "jakarta.inject.Singleton",
+        "io.qalipsis.api.annotations.StepConverter",
+        "io.qalipsis.api.annotations.StepDecorator",
+        "io.qalipsis.api.annotations.PluginComponent",
+        "io.qalipsis.api.annotations.Spec",
+        "io.micronaut.validation.Validated"
+    )
+}
+
+val micronautVersion: String by project
+val kotlinCoroutinesVersion: String by project
+val jacksonVersion: String by project
+val catadioptreVersion: String by project
+
+kotlin.sourceSets["test"].kotlin.srcDir("build/generated/source/kaptKotlin/catadioptre")
+kapt.useBuildCache = false
+
+logger.lifecycle("Using Micronaut $micronautVersion and Kotlin Coroutines $kotlinCoroutinesVersion")
+
+dependencies {
+    compileOnly("io.aeris-consulting:catadioptre-annotations:${catadioptreVersion}")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    compileOnly("io.micronaut:micronaut-runtime")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:$jacksonVersion")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+    api("io.qalipsis:api-common:${project.version}")
+    api("io.qalipsis:api-dsl:${project.version}")
+
+    kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kapt("io.qalipsis:api-processors:${project.version}")
+    kapt("io.qalipsis:api-dsl:${project.version}")
+    kapt("io.qalipsis:api-common:${project.version}")
+    kapt("io.aeris-consulting:catadioptre-annotations:${catadioptreVersion}")
+
+    testFixturesImplementation(kotlin("stdlib"))
+    testFixturesImplementation("io.qalipsis:api-common:${project.version}")
+    testFixturesImplementation("io.qalipsis:test:${project.version}")
+
+    testImplementation("io.qalipsis:test:${project.version}")
+    testImplementation("io.qalipsis:api-dsl:${project.version}")
+    testImplementation(testFixtures("io.qalipsis:api-dsl:${project.version}"))
+    testImplementation(testFixtures("io.qalipsis:api-common:${project.version}"))
+    testImplementation(testFixtures("io.qalipsis:runtime:${project.version}"))
+    testImplementation("javax.annotation:javax.annotation-api")
+    testImplementation("io.micronaut:micronaut-runtime")
+    testImplementation("io.aeris-consulting:catadioptre-kotlin:${catadioptreVersion}")
+    testRuntimeOnly("io.qalipsis:runtime:${project.version}")
+
+    kaptTest(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kaptTest("io.micronaut:micronaut-inject-java")
+    kaptTest("io.qalipsis:api-processors:${project.version}")
+}
