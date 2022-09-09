774a8c005afbdaaa81001b96016bdd9b5067c80e
Eric Jessé <me@ericjesse.com>
2022-09-09 18:07:31 +0200

    QALI-284 Create a BOM for the QALIPSIS developers
    This commit also add the licenses declaration on the project

--- elasticsearch/build.gradle.kts
+++ elasticsearch/build.gradle.kts
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2022 AERIS IT Solutions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 plugins {
     kotlin("jvm")
     kotlin("kapt")
     kotlin("plugin.allopen")
 }
 
-description = "Qalipsis Plugins - Elasticsearch"
+description = "QALIPSIS plugin for Elasticsearch"
 
 allOpen {
     annotations(
@@ -18,59 +34,53 @@ allOpen {
     )
 }
 
-val micronautVersion: String by project
-val kotlinCoroutinesVersion: String by project
-val testContainersVersion: String by project
-val jacksonVersion: String by project
-
-val elasticsearchVersion = "7.12.0"
-val catadioptreVersion: String by project
-
 kotlin.sourceSets["test"].kotlin.srcDir("build/generated/source/kaptKotlin/catadioptre")
 kapt.useBuildCache = false
 
+val coreVersion: String by project
+val elasticsearchVersion = "8.4.1"
+
 dependencies {
-    compileOnly("io.aeris-consulting:catadioptre-annotations:${catadioptreVersion}")
-    compileOnly(kotlin("stdlib"))
-    compileOnly(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    implementation(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    compileOnly("io.aeris-consulting:catadioptre-annotations")
 
     compileOnly("io.micronaut:micronaut-runtime")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     api("org.elasticsearch.client:elasticsearch-rest-client:$elasticsearchVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    api("io.qalipsis:api-common:${project.version}")
-    api("io.qalipsis:api-dsl:${project.version}")
+    api("io.qalipsis:api-common")
+    api("io.qalipsis:api-dsl")
     api("io.micronaut.micrometer:micronaut-micrometer-registry-elastic")
 
-    kapt("io.aeris-consulting:catadioptre-annotations:${catadioptreVersion}")
-    kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kapt(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    kapt("io.aeris-consulting:catadioptre-annotations")
     kapt("io.micronaut:micronaut-inject-java")
     kapt("io.micronaut:micronaut-validation")
     kapt("io.micronaut:micronaut-graal")
-    kapt("io.qalipsis:api-processors:${project.version}")
-    kapt("io.qalipsis:api-dsl:${project.version}")
-    kapt("io.qalipsis:api-common:${project.version}")
+    kapt("io.qalipsis:api-processors")
+    kapt("io.qalipsis:api-dsl")
+    kapt("io.qalipsis:api-common")
 
-    testImplementation("io.aeris-consulting:catadioptre-kotlin:${catadioptreVersion}")
-    testImplementation("org.testcontainers:elasticsearch:$testContainersVersion")
+    testImplementation(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    testImplementation("io.aeris-consulting:catadioptre-kotlin")
+    testImplementation("org.testcontainers:elasticsearch")
     testImplementation("io.micronaut.test:micronaut-test-junit5")
-    testImplementation("io.qalipsis:test:${project.version}")
-    testImplementation("io.qalipsis:api-dsl:${project.version}")
-    testImplementation(testFixtures("io.qalipsis:api-dsl:${project.version}"))
-    testImplementation(testFixtures("io.qalipsis:api-common:${project.version}"))
-    testImplementation(testFixtures("io.qalipsis:runtime:${project.version}"))
+    testImplementation("io.qalipsis:test")
+    testImplementation("io.qalipsis:api-dsl")
+    testImplementation(testFixtures("io.qalipsis:api-dsl"))
+    testImplementation(testFixtures("io.qalipsis:api-common"))
+    testImplementation(testFixtures("io.qalipsis:runtime"))
     testImplementation("javax.annotation:javax.annotation-api")
     testImplementation("io.micronaut:micronaut-runtime")
-    testRuntimeOnly("io.qalipsis:runtime:${project.version}")
-    testRuntimeOnly("io.qalipsis:head:${project.version}")
-    testRuntimeOnly("io.qalipsis:factory:${project.version}")
+    testRuntimeOnly("io.qalipsis:runtime")
+    testRuntimeOnly("io.qalipsis:head")
+    testRuntimeOnly("io.qalipsis:factory")
 
 
-    kaptTest(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kaptTest(platform("io.qalipsis:plugin-platform:${coreVersion}"))
     kaptTest("io.micronaut:micronaut-inject-java")
-    kaptTest("io.qalipsis:api-processors:${project.version}")
+    kaptTest("io.qalipsis:api-processors")
 }
 
 
--- elasticsearch/src/test/kotlin/io/qalipsis/plugins/elasticsearch/poll/PollScenario.kt
+++ elasticsearch/src/test/kotlin/io/qalipsis/plugins/elasticsearch/poll/PollScenario.kt
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2022 AERIS IT Solutions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package io.qalipsis.plugins.elasticsearch.poll
 
 import io.qalipsis.api.annotations.Scenario
+import io.qalipsis.api.executionprofile.regular
 import io.qalipsis.api.lang.concurrentList
-import io.qalipsis.api.rampup.regular
 import io.qalipsis.api.scenario.scenario
 import io.qalipsis.api.steps.filterNotNull
 import io.qalipsis.api.steps.innerJoin
@@ -37,7 +53,7 @@ object PollScenario {
     fun pollData() {
         scenario("elasticsearch-poll") {
             minionsCount = minions
-            rampUp {
+            profile {
                 // Starts all at once.
                 regular(100, minionsCount)
             }
@@ -68,17 +84,17 @@ object PollScenario {
             .logErrors()
             .map(ElasticsearchDocument<BuildingMove>::value)
             .innerJoin(
-                    using = { it.value.username },
-                    on = {
-                        it.elasticsearch()
-                            .poll {
-                                name = "poll.out"
-                                index("building-moves")
-                                client {
-                                    RestClient.builder(HttpHost("localhost", esPort, "http")).build()
-                                }
-                                query {
-                                    """
+                using = { it.value.username },
+                on = {
+                    it.elasticsearch()
+                        .poll {
+                            name = "poll.out"
+                            index("building-moves")
+                            client {
+                                RestClient.builder(HttpHost("localhost", esPort, "http")).build()
+                            }
+                            query {
+                                """
                                      {
                                         "query": {
                                             "term": {
@@ -89,14 +105,14 @@ object PollScenario {
                                         "sort":["timestamp","username"]
                                     }
                                     """.trimIndent()
-                                }
-                                pollDelay(Duration.ofSeconds(1))
                             }
-                            .flatten(BuildingMove::class)
-                            .logErrors()
-                            .map(ElasticsearchDocument<BuildingMove>::value)
-                    },
-                    having = { it.value.username }
+                            pollDelay(Duration.ofSeconds(1))
+                        }
+                        .flatten(BuildingMove::class)
+                        .logErrors()
+                        .map(ElasticsearchDocument<BuildingMove>::value)
+                },
+                having = { it.value.username }
             )
             .filterNotNull()
             .map {
