a78fa503362df902de90861790db343fbbffd8f5
Eric Jessé <me@ericjesse.com>
2023-02-16 10:43:45 +0100

    Updating platform versions

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -38,10 +38,10 @@ allOpen {
 kotlin.sourceSets["test"].kotlin.srcDir("build/generated/source/kaptKotlin/catadioptre")
 kapt.useBuildCache = false
 
-val coreVersion: String by project
+val pluginPlatformVersion: String by project
 
 dependencies {
-    implementation(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    implementation(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
     compileOnly("io.aeris-consulting:catadioptre-annotations")
     compileOnly("io.micronaut:micronaut-runtime")
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core")
@@ -52,17 +52,17 @@ dependencies {
     api("io.qalipsis:api-common")
     api("io.qalipsis:api-dsl")
 
-    kapt(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    kapt(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
     kapt("io.qalipsis:api-processors")
     kapt("io.qalipsis:api-dsl")
     kapt("io.qalipsis:api-common")
     kapt("io.aeris-consulting:catadioptre-annotations")
 
-    testFixturesImplementation(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    testFixturesImplementation(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
     testFixturesImplementation("io.qalipsis:api-common")
     testFixturesImplementation("io.qalipsis:test")
 
-    testImplementation(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    testImplementation(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
     testImplementation("io.qalipsis:test")
     testImplementation("io.qalipsis:api-dsl")
     testImplementation(testFixtures("io.qalipsis:api-dsl"))
@@ -75,7 +75,7 @@ dependencies {
     testRuntimeOnly("io.qalipsis:head")
     testRuntimeOnly("io.qalipsis:factory")
 
-    kaptTest(platform("io.qalipsis:plugin-platform:${coreVersion}"))
+    kaptTest(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
     kaptTest("io.micronaut:micronaut-inject-java")
     kaptTest("io.qalipsis:api-processors")
 }
