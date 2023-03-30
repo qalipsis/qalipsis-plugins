3e3bbac5d55ac5d09324fa3f2623f18a8e303baf
Joël Valère <joelvalere3@gmail.com>
2023-03-30 20:59:38 +0100

    QALI-443 Rename all the QALIPSIS modules

--- rabbitmq/build.gradle.kts
+++ rabbitmq/build.gradle.kts
@@ -42,38 +42,38 @@ val rabbitMQVersion = "5.16.0"
 val pluginPlatformVersion: String by project
 
 dependencies {
-    implementation(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
+    implementation(platform("io.qalipsis:qalipsis-plugin-platform:${pluginPlatformVersion}"))
     compileOnly("io.aeris-consulting:catadioptre-annotations")
     compileOnly(kotlin("stdlib"))
     compileOnly("io.micronaut:micronaut-runtime")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     api("com.rabbitmq:amqp-client:$rabbitMQVersion")
 
-    api("io.qalipsis:api-common")
-    api("io.qalipsis:api-dsl")
+    api("io.qalipsis:qalipsis-api-common")
+    api("io.qalipsis:qalipsis-api-dsl")
 
-    kapt(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
-    kapt("io.qalipsis:api-processors")
-    kapt("io.qalipsis:api-dsl")
-    kapt("io.qalipsis:api-common")
+    kapt(platform("io.qalipsis:qalipsis-plugin-platform:${pluginPlatformVersion}"))
+    kapt("io.qalipsis:qalipsis-api-processors")
+    kapt("io.qalipsis:qalipsis-api-dsl")
+    kapt("io.qalipsis:qalipsis-api-common")
     kapt("io.aeris-consulting:catadioptre-annotations")
 
     testImplementation("org.testcontainers:rabbitmq")
-    testImplementation("io.qalipsis:test")
-    testImplementation("io.qalipsis:api-dsl")
-    testImplementation(testFixtures("io.qalipsis:api-dsl"))
-    testImplementation(testFixtures("io.qalipsis:api-common"))
-    testImplementation(testFixtures("io.qalipsis:runtime"))
+    testImplementation("io.qalipsis:qalipsis-test")
+    testImplementation("io.qalipsis:qalipsis-api-dsl")
+    testImplementation(testFixtures("io.qalipsis:qalipsis-api-dsl"))
+    testImplementation(testFixtures("io.qalipsis:qalipsis-api-common"))
+    testImplementation(testFixtures("io.qalipsis:qalipsis-runtime"))
     testImplementation("javax.annotation:javax.annotation-api")
     testImplementation("io.micronaut:micronaut-runtime")
     testImplementation("io.aeris-consulting:catadioptre-kotlin")
-    testRuntimeOnly("io.qalipsis:runtime")
-    testRuntimeOnly("io.qalipsis:head")
-    testRuntimeOnly("io.qalipsis:factory")
+    testRuntimeOnly("io.qalipsis:qalipsis-runtime")
+    testRuntimeOnly("io.qalipsis:qalipsis-head")
+    testRuntimeOnly("io.qalipsis:qalipsis-factory")
 
-    kaptTest(platform("io.qalipsis:plugin-platform:${pluginPlatformVersion}"))
+    kaptTest(platform("io.qalipsis:qalipsis-plugin-platform:${pluginPlatformVersion}"))
     kaptTest("io.micronaut:micronaut-inject-java")
-    kaptTest("io.qalipsis:api-processors")
+    kaptTest("io.qalipsis:qalipsis-api-processors")
 }
 
 
