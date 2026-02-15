b7829387d0003f8b7916ca8df25c179443f340ec
Eric Jessé <me@ericjesse.com>
2026-02-15 21:07:03 +0100

    Improve the build to report the list of generated events and meters as resources

--- cassandra/build.gradle.kts
+++ cassandra/build.gradle.kts
@@ -75,7 +75,6 @@ dependencies {
     testFixturesImplementation("io.qalipsis:qalipsis-test")
 
     testImplementation(platform("io.qalipsis:qalipsis-plugin-platform:${pluginPlatformVersion}"))
-    testImplementation("org.testcontainers:testcontainers-bom:2.0.3")
     testImplementation("org.testcontainers:testcontainers-cassandra")
     testImplementation("io.qalipsis:qalipsis-test")
     testImplementation("io.qalipsis:qalipsis-api-dsl")
