80dd9519db8791d5fb4606f21282740d89152c7e
Eric Jessé <me@ericjesse.com>
2026-02-15 16:05:53 +0100

    Improve the build to report the list of generated events and meters as resources

--- influxdb/build.gradle.kts
+++ influxdb/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
 
     testImplementation(platform("io.qalipsis:qalipsis-plugin-platform:${pluginPlatformVersion}"))
     testImplementation("io.aeris-consulting:catadioptre-kotlin")
-    testImplementation("org.testcontainers:influxdb")
+    testImplementation("org.testcontainers:testcontainers-influxdb")
     testImplementation("io.micronaut.test:micronaut-test-junit5")
     testImplementation("io.qalipsis:qalipsis-test")
     testImplementation("io.qalipsis:qalipsis-api-dsl")
