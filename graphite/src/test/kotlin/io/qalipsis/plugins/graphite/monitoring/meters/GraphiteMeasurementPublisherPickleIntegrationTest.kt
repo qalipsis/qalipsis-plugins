8fe7f32a7e078f89481865f2283ece548773048a
Eric Jessé <me@ericjesse.com>
2026-04-23 15:03:51 +0200

    [Graphite] Fixes integration tests

---
+++ graphite/src/test/kotlin/io/qalipsis/plugins/graphite/monitoring/meters/GraphiteMeasurementPublisherPickleIntegrationTest.kt
@@ -0,0 +1,70 @@
+/*
+ * QALIPSIS
+ * Copyright (C) 2025 AERIS IT Solutions GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package io.qalipsis.plugins.graphite.monitoring.meters
+
+import io.netty.channel.ChannelOutboundHandlerAdapter
+import io.qalipsis.plugins.graphite.Constants
+import io.qalipsis.plugins.graphite.GraphiteProtocol
+import io.qalipsis.plugins.graphite.client.codecs.PickleEncoder
+import java.time.Duration
+import kotlin.math.pow
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+
+@Testcontainers
+internal class GraphiteMeasurementPublisherPickleIntegrationTest : AbstractGraphiteMeasurementPublisherProtocolTest() {
+
+    override val protocolPathIdentifier: String = "pickle"
+
+    override val recordEncoder: ChannelOutboundHandlerAdapter = PickleEncoder()
+
+    override val graphitePort: Int = Constants.GRAPHITE_PICKLE_PORT
+
+    override val protocol: GraphiteProtocol = GraphiteProtocol.PICKLE
+
+    override val container: GenericContainer<*> get() = CONTAINER
+
+    companion object {
+
+        @Container
+        @JvmStatic
+        private val CONTAINER = GenericContainer<Nothing>(
+            DockerImageName.parse(Constants.GRAPHITE_IMAGE_NAME)
+        ).apply {
+            withExposedPorts(Constants.HTTP_PORT, Constants.GRAPHITE_PICKLE_PORT)
+            setWaitStrategy(HttpWaitStrategy().forPort(Constants.HTTP_PORT).forPath("/render").forStatusCode(200))
+            withStartupTimeout(Duration.ofSeconds(60))
+
+            withCreateContainerCmdModifier { it.hostConfig!!.withMemory(512 * 1024.0.pow(2).toLong()).withCpuCount(2) }
+            withClasspathResourceMapping("carbon.conf", Constants.CARBON_CONFIG_PATH, BindMode.READ_ONLY)
+            withClasspathResourceMapping(
+                "storage-schemas.conf",
+                Constants.STORAGE_SCHEMA_CONFIG_PATH,
+                BindMode.READ_ONLY
+            )
+        }
+    }
+
+}
